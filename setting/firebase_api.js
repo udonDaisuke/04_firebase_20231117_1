90873be7f1284e7a13b7c04778e19513dc654fba
udonDaisuke <sato.must.breathe0101@gmail.com>
2023-11-20 11:16:23 +0900

    .gitignoreテスト

---
+++ setting/firebase_api.js
@@ -0,0 +1,10 @@
+// !FIREVASE API KEY
+export const firebaseConfig = {
+apiKey: "AIzaSyCSeB1vEGfBsyyzoxZTHmIADF8a03cbUcM",
+authDomain: "gsdev26us07-15604.firebaseapp.com",
+databaseURL: "https://gsdev26us07-15604-default-rtdb.firebaseio.com",
+projectId: "gsdev26us07-15604",
+storageBucket: "gsdev26us07-15604.appspot.com",
+messagingSenderId: "858354124365",
+appId: "1:858354124365:web:2c2ae7b0b49b9ed285be04"
+}
